c8d1c168c40fdad693286416da383ad3d45df857
lucas <lucassidpaula@gmail.com>
2024-09-19 19:38:18 -0300

    chore(cs.proj) estabelecendo uma referência íntegra entre os projetos para uma possivel reutilização de projetos

---
+++ Arquivo-exe/Program.cs
@@ -0,0 +1,10 @@
+﻿using sistema_de_estacionamento.models;
+
+
+
+using estacionar;
+Class1 Teste = new Class1();
+Teste.Render();
+
+
+Console.WriteLine("Hello, World!");
---
+++ Arquivo-exe/models/estacionamento.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sistema_de_estacionamento.models
+{
+    public class Estacionamento
+    {
+        
+    }
+}
